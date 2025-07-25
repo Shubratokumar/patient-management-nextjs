fb44acfc42cdb033ca9278495cdcf7c7405d03e0
Shubrato Kumar <shubratokumargharami920@gmail.com>
2025-07-25 10:58:20 +0600

    shadcn component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-xl text-white underline">Patient Management solution home here!</h1>
+      <Button>Click Here !</Button>
     </div>
   )
 }
